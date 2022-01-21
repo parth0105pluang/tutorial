d4c6736e9f416a9be8319aa31dfd32593f767b7a
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-01-21 13:50:44 +0530

    not assigning require

---
+++ alternate.js
@@ -0,0 +1,7 @@
+const num2 = 10
+const num1 = 5
+function addValues()
+{
+    console.log(`the sum is : ${num1 + num2 }`)
+}
+addValues()
--- modules.js
+++ modules.js
@@ -2,6 +2,7 @@
 // Modules - Encapsulated Code (Only share minimum)
 const names = require('./names')
 const sayHi = require("./utility")
+require('./alternate')
 sayHi('susan')
 sayHi(names.john)
 sayHi(names.peter)
